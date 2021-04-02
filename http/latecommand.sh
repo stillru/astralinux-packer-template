8d62bc4b793c7fe9a3d2a7fed1ff3aaa048265b9
Steve Illichevskiy <still.ru@gmail.com>
2021-04-02 13:43:20 +0300

    fix: Используется ядро 5.4 (fix #19)

--- http/latecommand.sh
+++ http/latecommand.sh
@@ -3,6 +3,7 @@ mkdir -p --mode=0700 /home/vagrant/.ssh && curl -Lo /home/vagrant/.ssh/authorize
 mkdir -p --mode=0700 /root/.ssh && curl -Lo /root/.ssh/authorized_keys "https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub" && chmod 0600 /root/.ssh/authorized_keys && chown -R root:root /root/.ssh
 sed -i "s/^#PermitRootLogin.*\$/PermitRootLogin prohibit-password/g" /etc/ssh/sshd_config
 sed -i "s/^#PasswordAuthentication.*\$/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+apt-get -y remove linux-image-4.15*-hardened
+apt-get -y install linux-image-5.4*-generic
 cp /var/logs/syslog /root/syslog-install.log
-apt-get --yes purge linux-image-4.15.3-2-hardened
 systemctl enable ssh
